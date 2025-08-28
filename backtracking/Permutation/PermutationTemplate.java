35277cdd03defaa8ac72cfb9fd669582e7a9c371
rosie98 <kjr2699@naver.com>
2025-08-29 08:54:27 +0900

    Reapply "feat(#6): Permutations With Repetition"
    This reverts commit 0e702bca8c66f3c8d359cbe0bfb5a9f7c02f62a1.

--- backtracking/Permutation/PermutationTemplate.java
+++ backtracking/Permutation/PermutationTemplate.java
@@ -1,6 +1,6 @@
-package backtracking;
+package backtracking.Permutation;
 
-public class CombinationTemplate {
+public class PermutationTemplate {
     static int[] selected;
     static boolean[] visited;
 
@@ -10,28 +10,26 @@ public class CombinationTemplate {
         selected = new int[3];
         visited = new boolean[nums.length];
 
-        backTracking(0, 0, 3, nums);
+        backTracking(0, 3, nums);
     }
 
-    public static void backTracking(int pick, int start, int count, int[] nums) {
+    public static void backTracking(int pick, int count, int[] nums) {
         if (pick == count) {
             for (int i = 0; i < selected.length; i++) {
                 System.out.print(nums[selected[i]] + " ");
             }
 
             System.out.println();
-
             return;
         }
 
-        for (int i = start; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 selected[pick] = i;
-                backTracking(pick + 1, i + 1, count, nums);
+                backTracking(pick + 1, count, nums);
                 visited[i] = false;
             }
         }
-
     }
 }
